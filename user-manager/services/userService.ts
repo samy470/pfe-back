bafd9fcc420347cf387df39a8212e9d6916953c7
samy470 <messaoudi.191931046501@gmail.com>
2026-03-25 18:55:50 +0100

    Update userService.ts

--- user-manager/services/userService.ts
+++ user-manager/services/userService.ts
@@ -6,6 +6,9 @@ import crypto from 'crypto';
 import { UserExistsError } from "../exceptions/customErrors";
 
 dotenv.config();
+if (!process.env.SENDGRID_API_KEY) {
+  throw new Error("Missing SendGrid API key");
+}
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 //register
